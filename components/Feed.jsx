e5015f96ddd77803015018804b03b6df04ef485c
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-02-15 23:48:49 +0530

    updated code with completed feed with user prompts, made get api to extract the user prompts - working great, made the funtionality to copy the prompt, made the search bar too

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,63 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+
+const PromptCardList = ( { data, handleTagClick } ) => {
+    return (
+        <div className="mt-16 prompt_layout">
+            {
+                data?.map( ( post ) => (
+                    <PromptCard
+                        key={ post._id }
+                        post={ post }
+                        handleTagClick={ handleTagClick }
+                    />
+                ) )
+            }
+        </div>
+    )
+
+};
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState( '' );
+    const [posts, setPosts] = useState( [] );
+
+    const handelSearchChange = ( e ) => {
+    }
+
+    useEffect( () => {
+        const fetchPosts = async () => {
+            const response = await fetch( '/api/prompt' );
+            const data = await response.json();
+            setPosts( data )
+        }
+        fetchPosts();
+    }, [] )
+
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form className="relative w-full flex-center flex-col">
+                <input
+
+                    type="text"
+                    placeholder="Search for a tag or username"
+                    value={ searchText }
+                    onChange={ handelSearchChange }
+                    required
+                    className="search_input peer"
+                />
+
+                <PromptCardList
+                    data={ posts }
+                    handleTagClick={ () => { } }
+                />
+
+            </form>
+        </section>
     )
 }
 
